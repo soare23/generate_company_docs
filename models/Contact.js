a4a8085f5622575b25e9884ea4b9962c925ef8be
soare23 <toma.mihai.sorin23@gmail.com>
2021-04-06 19:11:06 +0300

    built all needed models, needs testing

---
+++ models/Contact.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+
+// We export a function that defines the model.
+// This function will automatically receive as parameter the Sequelize connection object.
+module.exports = (sequelize) => {
+  sequelize.define('contact', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    phone: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+  });
+};
